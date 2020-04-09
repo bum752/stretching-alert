cca953ce86d138cb61bc2426260676bc4a07ee9f
bum752 <bum752@gmail.com>
2020-04-09 18:11:32 +0900

    fix 환경변수 기본값 사용 Github Actions 에서 정의되지 않은 secrets의 값을 빈 문자열로 넘겨줘 발생했던 문제 해결

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const { IncomingWebhook } = require('@slack/webhook');
-const { ALERT_MESSAGE: message = '<!here> 스트레칭 할 시간입니다.', WEBHOOK_URL } = process.env;
+const { ALERT_MESSAGE, WEBHOOK_URL } = process.env;
+const message = ALERT_MESSAGE || '<!here> 스트레칭 할 시간입니다.';
+
+if (!WEBHOOK_URL) {
+  throw new Error('Not exists WEBHOOK_URL.');
+}
+
 const webhook = new IncomingWebhook(WEBHOOK_URL);
 
 (async () => {
-  await webhook.send({
-    text: message,
-  });
+  try {
+    await webhook.send({
+      text: message
+    });
+  } catch (e) {
+    throw e;
+  }
 })();
